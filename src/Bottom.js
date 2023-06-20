e25fb1a5860551b915826dd83dd7fe2989a5437d
Pirvan Cosmin <pirvancosmin@outlook.com>
2023-06-20 20:03:38 +0300

    update

--- src/Bottom.js
+++ src/Bottom.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { useEffect, useState } from "react";
-
+import './App.css';
 
 function Bottom() {
-    
-    const[currentDate, setCurrentDate] = useState(new Date());
-
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentDate(new Date().getFullYear());
-      }, 1000);
-      return () => clearInterval(interval);
-    }, []);
-
     return(
-        <>
-        <bottom>
-            <h3 id="thisYear">{`© ${currentDate.toString()} Pirvan Cosmin. All Rights Reserved.`}</h3>
+        <bottom className="bottom">
+        {`© ${new Date().getFullYear()} Pirvan Cosmin. All Rights Reserved.`}
         </bottom>
-        </>
     );
 }
 export default Bottom;
