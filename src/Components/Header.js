d41c05bff55ac7d0236ac9df4dc5c7f08f1d03bc
Pirvan Cosmin <pirvancosmin@outlook.com>
2023-06-21 19:23:34 +0300

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import './styles.css';
 function Header() {
     return(
         <header>
-        <h1>Welcome to my portofolio</h1>
+        <h1 className="welcome">Welcome to my portofolio</h1>
         <ul className='list'>
             <li className="listitem"><a href="#about">About</a></li>
             <li className="listitem"><a href="#projectsH">Projects</a></li>
